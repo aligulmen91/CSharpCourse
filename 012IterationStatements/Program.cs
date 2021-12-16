8e105b84bc8a9adb3f870f1698dc932daad14f49
[Ali Gulmen] <aligulmen91@gmail.com>
2021-12-16 11:56:02 +0300

    Iteration Statements

---
+++ 012IterationStatements/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012IterationStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac=0;
+            /*
+            For Loops
+            For Each Loops
+            While Loops
+            Do-While Loops
+             */
+            for (var i=0; i<10; i++)
+            {
+                if (i%2==0) /* even numbers */
+                Console.WriteLine(i);
+            }
+
+
+
+
+            var isim = "Ali Gulmen"; /*strings are array of chars, so we can use it as isim[x]*/
+
+            foreach(var character in isim)
+            {
+                Console.WriteLine(character);
+            }
+
+
+
+
+            while (sayac < 10){
+                if (sayac % 2 == 1) /* odd numbers */
+                    Console.WriteLine(sayac);
+                sayac++;
+
+            }
+
+            sayac = 0;
+
+
+
+            do
+                {
+                Console.WriteLine(sayac);
+                    sayac++;
+                } while (sayac < 10);
+
+
+
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Type Your Name : ");
+             var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                Console.WriteLine("@Echo: " + input);
+            }
+
+        }
+
+
+
+
+       
+
+
+    }
+}
---
+++ 013RandomClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013RandomClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();  
+           for (int i = 0; i < 10; i++)
+            Console.WriteLine(random.Next(1,100));
+
+
+            Console.WriteLine((int)'a'); /*ASCII number of a*//*if we want to write some random chars;*/
+            for (int i = 0; i < 10; i++)
+                Console.Write((char)random.Next(97, 122));
+            Console.WriteLine("");
+
+            /*if we want to create a password from char array, we should do like this; */
+            var buffer = new char[10];
+
+            for (int i = 0; i < 10; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
