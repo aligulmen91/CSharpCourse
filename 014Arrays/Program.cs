3ff55cad3c27163211cbcc1a90ffee697b99c7c4
[Ali Gulmen] <aligulmen91@gmail.com>
2021-12-16 13:03:20 +0300

    Arrays and Lists added

---
+++ 014Arrays/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014ArraysNLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*There are two types of multidimension array
+             Rectangular and Jagged
+            rectangular 
+            x x x x
+            x x x x
+            x x x x
+
+            jagged
+            x x x x
+            x x x
+            x x x x x x
+            x x x x x
+
+             */
+
+            var numbers = new[] { 3, 9, 8, 13, 46, 32 };
+            var anotherarray = new int[3];
+            
+            //Length
+            Console.WriteLine("Length : " +numbers.Length);
+            
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 46);
+            Console.WriteLine("Index of 46 : " + index);
+
+            //clear
+            Array.Clear(numbers, 0, 1); /* array, starting index, length... they will be zero because of they are integer. */
+
+            //copy
+            Array.Copy(numbers, anotherarray, 3);
+            Console.WriteLine("Another Array is now :");
+            foreach(int i in anotherarray)
+                Console.WriteLine(i);
+
+
+            //sort
+            Array.Sort(anotherarray);
+            Console.WriteLine("Sorted array is now :");
+            foreach (int i in anotherarray)
+                Console.WriteLine(i);
+
+
+            //reverse
+            Array.Reverse(anotherarray);
+            Console.WriteLine("Reversed array is now :");
+            foreach (int i in anotherarray)
+                Console.WriteLine(i);
+
+            /*rectangular 2D array*/
+            var matrix = new int[3, 5]
+            {
+                { 1,2,3,4,5},
+                { 6,7,8,9,10},
+                { 11,12,13,14,15}
+            };
+            var element = matrix[0, 2];
+         //   Console.WriteLine(element);
+
+            /*rectangular 3D array*/
+            var colors = new int[3, 4, 5];
+
+            /* Jagged array */
+
+            var newArray = new int[3][]; /*we define 3 row for our jagged array, and we leave blank length*/
+            newArray[0] = new int[4]; /*and for each row, we define the length seperately*/
+            newArray[1] = new int[5];
+            newArray[2] = new int[2];
+        }
+    }
+}
---
+++ 015Lists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* arrays are fixed size, that means we can not change size of arrays after we create it
+             lists are dynamic size data structers, so most of the time we use lists */
+            
+            var numbers = new List<int>();
+            var numbers2 = new List<int>() { 1,2,3,4};
+
+            /*
+            Add()       : add an object to the list
+            AddRange()  : to add list of objects or arrays
+            Remove()    : remove from list
+            RemoveAt()  : removes objects given index
+            IndexOf()   : index of given object
+            Contains()  : contains or not
+            Count()     : count of list
+             */
+
+            numbers.Add(1);
+            numbers.AddRange(numbers2);
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine("Index of 1 : " + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1 : " + numbers.LastIndexOf(1)); /*we have three 1 in the array, so we may check last one*/
+
+            Console.WriteLine("Count :" + numbers.Count);
+
+            numbers.Remove(1); /*it will remove only first 1*/
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine("Now, lets check the list without number 1 :");
+            /*if we want to remove all 1, WE CAN NOT REMOVE WITH FOREACH LOOP, THE APPLICATION WILL BE CRASHED*/
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (i == 1)
+                    numbers.Remove(i);
+            }
+
+           
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        }
+    }
+}
