700c41f0629d67752e59062d8c923650a04bae2b
[Ali Gulmen] <aligulmen91@gmail.com>
2021-12-17 21:12:13 +0300

    Indexer Course Added

---
+++ 028Indexers/HttpCookie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _028Indexers
+{
+    public class HttpCookie
+    {
+
+        private Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+
+
+        public string this[string key]   /*this is indexer*/
+        {
+            get { 
+            return _dictionary[key];
+            }
+            set {
+            _dictionary[key] = value;
+            }
+        }
+
+    }
+}
---
+++ 028Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _028Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Ali";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
