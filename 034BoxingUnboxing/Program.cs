92fae868edff47da4c6d9cc72a7271dbf3fabb95
[Ali Gulmen] <aligulmen91@gmail.com>
2021-12-18 15:12:36 +0300

    Boxing N Unboxing Courses Added

---
+++ 034BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _034BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Boxing*/
+            int number = 10;
+            object obj = number;
+
+            //or
+            object obj2 = 10;
+
+            /*Unboxing*/
+            int number2 = (int)obj;
+            /*And and boxing and Unboxing*/
+            /*
+             have a performance penalty - because of extra obj creation
+             be avoid if possible
+             
+             */
+
+        }
+    }
+}
